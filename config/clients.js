89d718e694eee79fbb6b9b46b349559275e713ff
zainfaisal220 <zainfaisal220@gmail.com>
2026-09-15 20:12:43 +0500

    Remove 4th gallery item from locks.ch

--- config/clients.js
+++ config/clients.js
@@ -440,8 +440,7 @@ window.CLIENTS_DATABASE = {
     gallery: [
       { img: "assets/img2.PNG", labelEN: "Locks" },
       { img: "assets/new_dreads.png", labelEN: "Locks Twists" },
-      { img: "assets/img5.jpg", labelEN: "Locks" },
-      { img: "assets/new_owner.png", labelEN: "Artist" }
+      { img: "assets/img5.jpg", labelEN: "Locks" }
     ]
   }
 };
